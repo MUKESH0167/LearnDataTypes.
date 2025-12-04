c3fd75daf8b6612ba124edf6f43610cdfce454b4
MUKESH0167 <hsekum999@gmail.com>
2025-12-04 14:53:10 +0530

    Add files via upload
    Learndatatypes

---
+++ Activity.java
@@ -0,0 +1,15 @@
+package mukesh;
+
+public class Activity {
+
+	public static void main(String[] args) {
+		//int
+		int ageOfPerson=30;
+		char logo='t';
+		String name="Mukesh";
+		boolean value=true;
+		System.out.println(name);
+
+	}
+
+}
